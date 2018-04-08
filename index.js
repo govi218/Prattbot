9257f4517da7f142cf110dd901ac36b848709ec9
Govind Mohan <govi218mu@gmail.com>
2018-04-07 21:14:22 -0400

    added res handler

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const processMessage = require('./processMessage');
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -22,6 +24,10 @@ app.post('/webhook', (req, res) => {
         // will only ever contain one message, so we get index 0
         let webhook_event = entry.messaging[0];
         console.log(webhook_event);
+                
+        // Get the sender PSID
+        let sender_psid = webhook_event.sender.id;
+        console.log('Sender PSID: ' + sender_psid);  
       });
   
       // Returns a '200 OK' response to all requests
@@ -60,3 +66,18 @@ app.get('/webhook', (req, res) => {
       }
     }
   });
+
+  // Handles messages events
+function handleMessage(sender_psid, received_message) {
+
+}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+
+}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+  
+}
